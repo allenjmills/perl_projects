a254c1faa4cf06227d009bcd84da5ce1fbfb3d2d
AJ Mills <mills.allen.j@gmail.com>
2015-11-14 15:06:21 -0500

    added hello_world.pl and wemoControl.pl

---
+++ hello_world.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $name = "AJ";
+print "hi $name! \n";
---
+++ wemoControl.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use WebService::Belkin::WeMo::Discover;
+
+my $wemoDiscover = WebService::Belkin::WeMo::Discover->new();
+
+# Perfrom UPNP Search for all Belkin WeMo switches
+my $discovered = $wemoDiscover->search();
+
+# Save device info to make API calls faster - eliminates search on startup
+#
+# Load from storage
+#my $discovered = $wemoDiscover->load("/etc/belkin.db");
+
+foreach my $ip (keys %{$discovered}) {
+	print "IP = $ip\n";
+	print "Friendly Name = $discovered->{$ip}->{'name'}\n";
+}
